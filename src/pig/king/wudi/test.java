a5bb58f121df7901f5d52a4e1e22bbcf57e2509d
Administrator <Administrator@PC-20161219VXCR>
2016-12-19 16:38:14 +0800

    commit changes

---
+++ src/pig/king/wudi/test.java
@@ -0,0 +1,10 @@
+package pig.king.wudi;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("pig king wudi");
+	}
+
+}
